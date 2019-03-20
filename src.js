72ee7280e7a5b14f92c8737ee5fff971016a6936
rugk <rugk+git@posteo.de>
2019-03-20 16:57:03 +0100

    Modernize JS & export EmojiMart as function

--- src.js
+++ src.js
@@ -1,27 +1,44 @@
-const NimblePicker = require('emoji-mart/dist/components/picker/nimble-picker').default
-const React = require('react')
-const { define } = require('remount/es6')
+import { Picker } from 'emoji-mart/dist-modern/index.js'
+import React from 'react'
+import { define } from 'remount/es6'
 
-async function main() {
-  const data = await (await fetch('node_modules/emoji-mart/data/all.json')).json()
-  const Picker = props => (React.createElement(NimblePicker, {
+function main(properties) {
+  console.log("given props", properties);
 
-    data,
-    native: false,
-    onSelect: emoji => console.log('emoji selected', emoji),
-    title: 'Emoji',
-    showPreview: true,
-    ...props
-  }))
+  const PickerPrepared = (interactiveProperties) => {
+    // filter null values
+    for (const element in interactiveProperties) {
+      if (interactiveProperties[element] === null) {
+        delete interactiveProperties[element];
+      }
+    }
+
+    console.log("new props:", interactiveProperties);
+
+    return React.createElement(Picker, {
+      // defaults
+      set: 'twitter',
+      native: false,
+      onSelect: emoji => console.log('emoji selected', emoji),
+      title: 'Emoji',
+      showPreview: false,
+      showPreview: true,
+      // passed to function
+      ...properties,
+      // manually set overwrite everything finally
+      ...interactiveProperties
+    });
+  }
 
   define({ 'emoji-picker': {
-    component: Picker,
+    component: PickerPrepared,
     attributes: ['set', 'native', 'title', 'showPreview']
-  }})
+  }});
 
   const picker = document.createElement('emoji-picker')
   document.body.appendChild(picker)
 }
 
-main().catch(err => console.error(err))
+// export as global
+window.emojiMart = main;
 
